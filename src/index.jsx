eaa01135d1a63598e2560a86712f760ee4122016
tetuanka <terhikamula@gmail.com>
2024-01-11 10:25:22 +0200

    workflow

--- src/index.jsx
+++ src/index.jsx
@@ -3,4 +3,7 @@ import ReactDOM from 'react-dom'
 import App from './App'
 import './styles.css'
 
-ReactDOM.render(<App />, document.getElementById('app'))
+//ReactDOM.render(<App />, document.getElementById('app'))
+const root = document.getElementById('root');
+const rootElement = React.createRoot(root);
+rootElement.render(<App />);
