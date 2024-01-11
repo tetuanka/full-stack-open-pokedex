7d81d8debec2dfb321889985d19ac906923bc8cc
tetuanka <terhikamula@gmail.com>
2024-01-11 10:20:35 +0200

    workflow

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
 
-// eslint-disable-next-line no-console
 app.listen(PORT, () => {
-  console.log('server started on port 5000')
+  console.log('server started on port 5000') // eslint-disable-line no-console
 })
