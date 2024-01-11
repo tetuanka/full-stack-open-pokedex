30cf047c84ce191e8f1f85ed6fc12375e652c7c2
tetuanka <terhikamula@gmail.com>
2024-01-11 19:14:23 +0200

    e2e

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ const express = require('express')
 const app = express()
 
 // Heroku dynamically sets a port
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 3000
 
 app.use(express.static('dist'))
 
 app.listen(PORT, () => {
-  console.log('server started on port 5000') // eslint-disable-line no-console
+  console.log('server started on port ${PORT}') // eslint-disable-line no-console
 })
--- src/index.jsx
+++ src/index.jsx
@@ -2,9 +2,6 @@ import React from 'react'
 import App from './App'
 import './styles.css'
 
-
-//ReactDOM.render(<App />, document.getElementById('app'))
-
 import { createRoot } from 'react-dom/client'
 const container = document.getElementById('app')
 const root = createRoot(container)
