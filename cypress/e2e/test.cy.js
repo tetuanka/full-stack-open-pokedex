9c1b6351426f413a32c5c96763956a564ff72cb0
tetuanka <terhikamula@gmail.com>
2024-01-11 13:33:00 +0200

    e2e

--- cypress/e2e/test.cy.js
+++ cypress/e2e/test.cy.js
@@ -1,7 +1,7 @@
 describe('Pokedex', function() {
   it('front page can be opened', function() {
-    cy.visit('http://localhost:8081')
-    cy.contains('ivysaur')
-    cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
+    test.cy.visit('http://localhost:5000')
+    test.cy.contains('ivysaur')
+    test.cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
 })
