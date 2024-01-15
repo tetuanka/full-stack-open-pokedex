63991b8313e90be5a3d0fd98a3f3b942752b5201
tetuanka <terhikamula@gmail.com>
2024-01-15 09:06:03 +0200

    e2e

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,8 @@ module.exports = {
     "browser": true,
     "es6": true,
     "jest/globals": true,
-    'node': true
+    'node': true,
+    "cypress/globals": true
   },
   "extends": [
     "eslint:recommended",
@@ -17,7 +18,7 @@ module.exports = {
     "sourceType": "module"
   },
   "plugins": [
-    "react", "jest", "eslint-comments"
+    "react", "jest", "eslint-comments", "cypress"
   ],
   "rules": {
     "indent": [
